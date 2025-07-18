3c2053573e2641db97564f46edcce47f487143b7
mcxiaochenn <mcxiaochenn.yyds@gmail.com>
2025-07-18 18:28:22 +0800

    添加网站地图

--- blog/anzhiyu/random.js
+++ blog/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3eeb/","posts/ae3/"];function toRandomPost(){
+var posts=["posts/ae3/","posts/3eeb/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
